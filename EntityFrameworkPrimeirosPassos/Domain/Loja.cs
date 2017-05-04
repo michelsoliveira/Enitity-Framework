eaa326b2c64c3409e842e4cb865a3210e1ccc85c
Michel Oliveira <michel.s.oliveira@gmail.com>
2017-05-04 11:46:44 -0300

    Adicionado as data annotations nas classes loja e produto;

--- EntityFrameworkPrimeirosPassos/Domain/Loja.cs
+++ EntityFrameworkPrimeirosPassos/Domain/Loja.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,12 @@ namespace Domain
     [Table("Loja")]
     public class Loja
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(200)]
         public string Nome { get; set; }
+        [MaxLength(2000)]
         public string Descricao { get; set; }
     }
 }
--- EntityFrameworkPrimeirosPassos/Domain/Produto.cs
+++ EntityFrameworkPrimeirosPassos/Domain/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,16 @@ namespace Domain
     [Table("Produto")]
     public class Produto
     {
+        [Key]//Define o atributo chave
         public int Id { get; set; }
+
+        [MaxLength(200)]//Limita o tamanho máximo
+        [Required]//Define o atributo como NotNull(não vazio)
         public string Nome { get; set; }
+        [MaxLength(2000)]//Limita o tamanho máximo
         public string Descricao { get; set; }
+        [Range(-999999999999.99, 999999999999.99)]//Define o tamanho minimo e maximo do valor
+        [Required]
         public decimal Valor { get; set; }
     }
 }

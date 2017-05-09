cea452b1f1ccbf0e7fc75fc8affbf49045ef2b96
Michel Oliveira <michel.s.oliveira@gmail.com>
2017-05-09 12:23:06 -0300

    Criação do form; Criação do action handler do botão; Instanciar o db context no form;

---
+++ EntityFrameworkPrimeirosPassos/WinForms/Form1.cs
@@ -0,0 +1,35 @@
+﻿using Data;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms
+{
+    public partial class Form1 : Form
+    {
+
+        private ProductDbContext context;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            context = new ProductDbContext();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Loja loja = new Loja()
+            {
+                Nome = "Loja Teste DevMedia"
+            };
+        }
+    }
+}

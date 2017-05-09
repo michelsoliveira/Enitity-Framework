29e5069de8fef1b82a51a39faebafb2f479c6586
Michel Oliveira <michel.s.oliveira@gmail.com>
2017-05-09 12:28:34 -0300

    comentários form; instancia de objetos loja e produto;

--- EntityFrameworkPrimeirosPassos/WinForms/Form1.cs
+++ EntityFrameworkPrimeirosPassos/WinForms/Form1.cs
@@ -15,21 +15,32 @@ namespace WinForms
     public partial class Form1 : Form
     {
 
-        private ProductDbContext context;
+        private ProductDbContext context;//Instancia o dbcontext
 
         public Form1()
         {
             InitializeComponent();
 
-            context = new ProductDbContext();
+            context = new ProductDbContext();//inicializa o ProductDbContext
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Loja loja = new Loja()
+            Loja loja = new Loja()//instancia um novo objeto loja
             {
-                Nome = "Loja Teste DevMedia"
+                Nome = "Loja Teste DevMedia"//parametro nome recebe o valor
             };
+            context.Lojas.Add(loja);//adiciona o objeto loja no context
+
+            Produto produto = new Produto()
+            {
+                Nome = "Produto de teste DevMedia",
+                Valor = 20m,
+                Loja = loja
+            };
+            context.Produtos.Add(produto);
+
+            context.SaveChanges();//salva as alterações no context
         }
     }
 }
